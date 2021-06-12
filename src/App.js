f8eb0f8a960058c0318265d7cb64522b531452d0
philhaynes337 <71613755+philhaynes337@users.noreply.github.com>
2021-06-12 14:13:00 -0500

    Coming Soon Landing Page
    Coming Soon Lander Page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import ComingSoon from './coming_soon/ComingSoon';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ComingSoon />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/coming_soon/ComingSoon.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './ComingSoon.css';
+
+class ComingSoon extends Component {
+
+    render() {
+
+        return(
+            <div className='comingSoon'>
+                
+                <h1>Coming Soon</h1>
+                    
+                        
+                   
+                            
+
+                                <div className="circle center">
+
+                            
+                        
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ComingSoon
